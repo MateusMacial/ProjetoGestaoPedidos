60bdfcd955074adf08066987e7a2f1d051d4fae5
mateusmacial <mateus_macial@hotmail.com>
2021-12-16 13:59:56 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Projeto Gestão de Pedidos</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item to="/">
+          <q-item-section>
+            <q-item-label>Home</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/produtoList">
+          <q-item-section>
+            <q-item-label>Produtos</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  components: {},
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ src/pages/produtoList.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md">
+      <q-table
+        title="Produtos"
+        :data="data"
+        :columns="columns"
+        row-key="codProduto"
+        selection="multiple"
+        :selected.sync="selected"
+        :filter="filter"
+      >
+
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+      </q-table>
+
+      <br>
+
+      <q-btn padding:15px round color="primary" icon="add" @click="addProduto" />
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: [],
+      filter: '',
+      columns: [{
+        name: 'codProduto',
+        required: true,
+        label: 'Código Produto',
+        field: 'codProduto',
+        align: 'left',
+        sortable: true
+      },
+      {
+        name: 'nomeProduto',
+        required: true,
+        label: 'Nome Produto',
+        field: 'nomeProduto',
+        align: 'left',
+        sortable: true
+      }
+      ],
+      data: [
+        {
+          codProduto: '0124',
+          nomeProduto: 'Banana'
+        },
+        {
+          codProduto: '4243245',
+          nomeProduto: 'Maça'
+        },
+        {
+          codProduto: '23424',
+          nomeProduto: 'Uva'
+        }
+      ]
+    }
+  },
+  methods: {
+    addProduto () {
+      console.log(this.selected)
+    }
+  }
+}
+</script>
